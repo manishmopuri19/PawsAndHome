8edf57b68acff0fcd8a4df068690829c9c578a33
manishmopuri19 <manishsreeram2@gmail.com>
2026-03-01 23:30:45 +0530

    Resolved contact issue

--- FrontEnd/Script/contact.js
+++ FrontEnd/Script/contact.js
@@ -5,16 +5,16 @@ const API_URL = "https://pawsandhome.onrender.com/api/contact";
 document.getElementById("contactForm").addEventListener("submit",async(e)=>{
     
         e.preventDefault();
-        const name=document.getElementById("repoterName").value;
+        const userName=document.getElementById("repoterName").value;
         const email=document.getElementById("repoterEmail").value;
         const subject=document.getElementById("reportSubject").value;
-        const note=document.getElementById("reportNote").value;
+        const Message=document.getElementById("reportNote").value;
 
          const data={
-            userName:name,
-            email:email,
-            subject:subject,
-            Message:note
+            userName,
+            email,
+            subject,
+            Message
         }
     try {
         const res= await fetch(`${API_URL}/reportAdmin`,{
